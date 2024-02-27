e3f4f96f93e5677b5f3618e8bd1037c4bcbb3c19
Murat Berat Basari <muratberatbasari@gmail.com>
2024-02-27 07:31:02 +0000

    v7

---
+++ src/main/java/com/project/service/business/StudentController.java
@@ -0,0 +1,67 @@
+package com.project.service.business;
+
+import com.project.schoolmanagment.payload.request.businnes.ChooseLessonProgramRequest;
+import com.project.schoolmanagment.payload.request.user.StudentRequest;
+import com.project.schoolmanagment.payload.request.user.StudentUpdateRequestWithoutPassword;
+import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
+import com.project.schoolmanagment.payload.response.user.StudentResponse;
+import com.project.schoolmanagment.service.user.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/student")
+@RequiredArgsConstructor
+public class StudentController {
+
+  @PreAuthorize("hasAnyAuthority('Admin')")
+  @PostMapping("/save")
+  private final StudentService studentService;
+
+
+  public ResponseEntity<ResponseMessage<StudentResponse>>saveStudent(
+      @RequestBody @Valid StudentRequest studentRequest){
+    return ResponseEntity.ok(studentService.saveStudent(studentRequest));
+  }
+
+
+
+
+
+
+
+  @PreAuthorize("hasAnyAuthority('Student')")
+  @PatchMapping("/update")
+  public ResponseEntity<String>updateStudent(@RequestBody @Valid
+      StudentUpdateRequestWithoutPassword studentUpdateRequestWithoutPassword,
+      HttpServletRequest request){
+    return studentService.updateStudent(studentUpdateRequestWithoutPassword,request);
+  }
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+  @PutMapping("/update/{userId}")
+  public ResponseMessage<StudentResponse>updateStudentForManagers(
+      @PathVariable Long userId,
+      @RequestBody @Valid StudentRequest studentRequest){
+    return studentService.updateStudentByManagers(userId,studentRequest);
+  }
+
+  @PreAuthorize("hasAnyAuthority('Student')")
+  @PostMapping("/addLessonProgramToStudent")
+  public ResponseMessage<StudentResponse>addLessonProgram(HttpServletRequest httpServletRequest,
+      @RequestBody @Valid ChooseLessonProgramRequest lessonProgramRequest){
+    return studentService.addLessonProgram(httpServletRequest,lessonProgramRequest);
+  }
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+  @GetMapping("/changeStatus")
+  public ResponseMessage changeStatus(@RequestParam Long id,@RequestParam boolean status){
+    return studentService.changeStatus(id,status);
+  }
+
+}
